9e5aee35e7a05eec3f3f15390ffdca9597d01c55
Luis Carlos - [front] <luisestrela.bol951@gmail.com>
2024-05-31 20:23:08 -0300

    feat: add react and tailwind

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@nemesis-ui/tokens';
-
-console.log(colors);
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import './styles/tailwind.css';
+
+export default function App() {
+  return <h1 className="text-red-700">hello world</h1>;
+}
